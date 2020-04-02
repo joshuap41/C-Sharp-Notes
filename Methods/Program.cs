7483f476fa92eb31482f85033170fa8b4e97f124
Joshua Johnson <joshua.j@outlook.com>
2020-04-02 09:58:59 -0400

    Inital Upload

---
+++ Methods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VariablesAndMethods
+{
+    class Program
+    {
+        //static => nothing is needed to run the method
+        //void => doesn't return anything
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(Add(5, 5));
+
+            AddSecond(10, 10);
+
+            Console.ReadLine();
+        }
+
+        //a way of using a method to "return" a value.
+        private static int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        //this way of using a method doesn't "return" a value but it takes care of the entire process.
+        //everything inside of this method only exists inside this method Via "void"
+        private static void AddSecond(int n1, int n2)
+        {
+            int result;
+            result = n1 + n2;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ VariablesMethodsChallenge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace VariablesMethodsChallenge
+{
+    class Program
+    {
+        //class level variables
+        static int n1 = 10, n2 = 20;
+        private static void Main(string[] args)
+        {
+            var result = Add();
+            var resultNeg = Subtract();
+            var resultMultiply = Multiply();
+            var resultDivide = Divide();
+
+            Console.WriteLine(result);
+            Console.WriteLine(resultNeg);
+            Console.WriteLine(resultMultiply);
+            Console.WriteLine(resultDivide);
+
+            Console.ReadLine();
+        }
+
+        private static int Add()
+        {
+            return n1 + n2;
+        }
+
+
+        private static int Subtract()
+        {
+            return n1 - n2;
+        }
+
+        private static int Multiply()
+        {
+            return n1 * n2;
+        }
+
+        private static int Divide()
+        {
+            return n1 / n2;
+        }
+    }
+}
